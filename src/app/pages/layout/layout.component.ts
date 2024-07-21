31e8cb4c6534289a1a8ba28a676b314138bdd2d3
Mehdi Pourhasan <mprhasn@gmail.com>
2024-07-21 17:07:55 +0330

    footer completed

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  loggedInUser: any;
+  router = inject(Router);
+
+  constructor() {
+    const localData = localStorage.getItem('complaintUser');
+    if (localData != null) {
+      this.loggedInUser = JSON.parse(localData);
+    }
+  }
+
+  logoff() {
+    localStorage.removeItem('complaintUser');
+    this.router.navigateByUrl('/login');
+  }
+}
